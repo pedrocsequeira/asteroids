a023504444293cc89e2dc796ae434a217abb2d7b
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-25 14:59:25 +0100

    added framrate limiter at 60 fps

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}')
 
     pygame.init()
+    
+    clock = pygame.time.Clock()
+
+    dt = 0 
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill('black')
         
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick()/1000
 
 if __name__ == "__main__":
     main()
