708e2371f8b568701e6c4e5182394aaf930763b7
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-25 15:41:39 +0100

    created player class and displayed the sprite in game

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}')
 
     pygame.init()
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0 
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill('black')
-        
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
